ab75d36d5d76fc56422a1b9b19cdc72177d4e3cc
mandi andrade <mandi.andrade20@gmail.com>
2023-05-04 13:19:53 -0500

    built out framework for CustomerInfo

---
+++ src/components/CustomerInfo/CustomerInfo.jsx
@@ -0,0 +1,14 @@
+
+function CustomerInfo() {
+    
+    return (
+        <>
+            <h3>Step 2: Customer Information</h3>
+            {/* need customer info form imported here */}
+                {/* including radio buttons */}
+            {/* need button NEXT for moving to another page */}
+        </>
+    )
+}
+
+export default CustomerInfo;
